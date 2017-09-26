bce7a4fbd8d5270312fbb7b05f306870616a40ea
Ben Robaidek <brobaidek@gmail.com>
2017-09-26 12:49:38 -0700

    Fix cuda device bug

--- model/train.py
+++ model/train.py
@@ -16,7 +16,7 @@ def main():
     args = parseParams()
     if int(torch.cuda.is_available()) == 1:
         print('Device', torch.cuda.current_device())
-        os.environ['CUDA_VISIBLE_DEVICES'] = 1
+        os.environ['CUDA_VISIBLE_DEVICES'] = '1'
         print('Device', torch.cuda.current_device())
     if not os.path.isdir(args.save_path_full):
         train(args)
