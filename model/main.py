03c847ce305a9dc3f27cf8b27842dd181534a9fa
Ben Robaidek <brobaidek@gmail.com>
2017-09-26 18:43:26 -0700

    Catch memory out of bounds error

--- model/main.py
+++ model/main.py
@@ -16,7 +16,7 @@ rand = True
 net_type = ('lstm', 'gru')
 #lr = (.001, .002)
 epochs = 100,
-bs = 64,
+bs = 16, 64
 opt = ('adamax',)# 'adam', 'sgd')
 num_lay =  (1, 2, 4)
 hs = (100, 300, 500, 750, 1000, 2000)
@@ -35,22 +35,19 @@ try:
         if not (embfix and not ptemb):
             print(('Training: (net_type=%s, epoch=%d, bs=%d, opt=%s, num_lay=%d, hs=%d, num_dir=%d, embdim=%d, embfix=%s, ptemb=%s, dropout=%.1f})') %
                 (net_type, epoch, bs, opt, num_lay, hs, num_dir, embdim, embfix, ptemb, dropout))
-            #try:
-            subprocess.call(['python train.py' + \
-                    ' -net-type=' + str(net_type) + \
-                    #' -lr=' + str(lr) + \
-                    ' -epochs=' + str(epochs[0]) + \
-                    ' -batch-size=' + str(bs) + \
-                    ' -opt=' + opt + \
-                    ' -num-layers=' + str(num_lay) + \
-                    ' -hidden-sz=' + str(hs) + \
-                    ' -num-dir=' + str(num_dir) + \
-                    ' -emb-dim=' + str(embdim) + \
-                    ' -embfix=' + str(embfix) + \
-                    ' -pretr-emb=' + str(ptemb) + \
-                    ' -dropout=' + str(dropout)])
-            #except OSError:
-            #    print("Oops!  Ran out of memory.  Try again...")
+            os.system('python train.py' + \
+                        ' -net-type=' + str(net_type) + \
+                        #' -lr=' + str(lr) + \
+                        ' -epochs=' + str(epochs[0]) + \
+                        ' -batch-size=' + str(bs) + \
+                        ' -opt=' + opt + \
+                        ' -num-layers=' + str(num_lay) + \
+                        ' -hidden-sz=' + str(hs) + \
+                        ' -num-dir=' + str(num_dir) + \
+                        ' -emb-dim=' + str(embdim) + \
+                        ' -embfix=' + str(embfix) + \
+                        ' -pretr-emb=' + str(ptemb) + \
+                        ' -dropout=' + str(dropout)])
             os.system('sort -o ./saved_models/best_models.txt ./saved_models/best_models.txt')
 except(KeyboardInterrupt, SystemExit):
     sys.exit("Interrupted by ctrl+c\n")
