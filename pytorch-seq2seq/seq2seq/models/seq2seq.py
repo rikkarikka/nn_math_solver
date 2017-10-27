a665b46088df70e1f63850b3a29c49a58e420c3a
Ben Robaidek <brobaidek@gmail.com>
2017-10-27 07:24:01 -0700

    seq2seq

--- pytorch-seq2seq/seq2seq/models/seq2seq.py
+++ pytorch-seq2seq/seq2seq/models/seq2seq.py
@@ -45,8 +45,9 @@ class Seq2seq(nn.Module):
 
     def forward(self, input_variable, input_lengths=None, target_variable=None,
                 teacher_forcing_ratio=0):
-        print('There is a problem here, forward')
+
         encoder_outputs, encoder_hidden = self.encoder(input_variable, input_lengths)
+        print('There is a problem here, forward')
         result = self.decoder(inputs=target_variable,
                               encoder_hidden=encoder_hidden,
                               encoder_outputs=encoder_outputs,
