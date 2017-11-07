d1bc471902d9382fb4a206f4adb2fd47e02b6afb
Ben Robaidek <benro@uw.edu>
2017-11-07 12:21:10 -0800

    translate

--- tencent/translate.sh
+++ tencent/translate.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python ../OpenNMT-py/translate.py -gpu 0 -model ./models/Math23K-model_acc_81.39_ppl_1.65_e11.pt -src ./data/src-val.txt -tgt ./data/tgt-val.txt -replace_unk -verbose -output ./pred.txt
+python ../OpenNMT-py/translate.py -gpu 0 -model ./models/Math23K-model_acc_89.79_ppl_1.42_e23.pt -src ./data/src-val.txt -tgt ./data/tgt-val.txt -replace_unk -verbose -output ./pred.txt
