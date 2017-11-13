2171f288ab6fab63e132acaa90f0915abc968bba
Ben Robaidek <brobaidek@gmail.com>
2017-11-13 13:22:58 -0800

    tencent preprocessing

--- tencent/data/preprocess.py
+++ tencent/data/preprocess.py
@@ -6,18 +6,48 @@ import re
 import sys
 
 def main():
-    crossValidation('./Math23K.json', './Math23K-train.txt', './Math23K-dev.txt', './Math23K-test.txt', k = 5, k_test=5)
-    #split('./Math23K.json', './Math23K-train.txt', './Math23K-dev.txt', './Math23K-test.txt')
-    json2txt('./Math23K-train.txt','./Math23K.json',   './src-train.txt',  './tgt-train.txt')
-    json2txt('./Math23K-dev.txt','./Math23K.json',     './src-val.txt',    './tgt-val.txt')
-    json2txt('./Math23K-test.txt','./Math23K.json',    './src-test.txt',   './tgt-test.txt')
 
-def crossValidation(json_path, train_path, dev_path, test_path, k = 5, k_test=5):
+    # LOAD DATA
+    data = json.loads(open('./Math23K.json').read())
+    random.shuffle(data)
+
+    # PREPROCESS DATA
+    for d in data:
+        d['segmented_text'], d['equation'] = preprocess(d['segmented_text'], d['equation'])
+
+    # 5 FOLD CROSS VALIDATION
+    print('Preforming cross validation splits...')
+    crossValidation(data, './Math23K-train.txt', './Math23K-dev.txt', './Math23K-test.txt', k = 5, k_test=5)
+
+    # SAVE SRC/TGT files
+    train_indices = np.genfromtxt('./Math23K-train.txt').astype(int)
+    dev_indices = np.genfromtxt('./Math23K-dev.txt').astype(int)
+    test_indices = np.genfromtxt('./Math23K-test.txt').astype(int)
+    json2txt(train_indices, data,   './src-train.txt',  './tgt-train.txt')
+    json2txt(dev_indices,   data,   './src-val.txt',    './tgt-val.txt')
+    json2txt(test_indices,  data,   './src-test.txt',   './tgt-test.txt')
+
+    # REMOVE TEST FOLD BEFORE COUNTING UNCOMMON EQUATIONS
+    print('len(data):',len(data))
+    print('len(test_indices):',len(test_indices))
+    data = [d for d in data if int(d['id']) not in test_indices]
+
+    # REMOVE UNCOMMON EQUATIONS
+    print('Removing uncommon equations...')
+    print('Started with', len(data), 'examples')
+    common_data, uncommon_data = mostCommon(data, .8)
+    print('Filtered down to', len(common_data), 'examples')
+
+    # SAVE SRC/TGT files (FILTERED DATA)
+    train_dev_indices = np.append(train_indices, dev_indices)
+    json2txt(train_dev_indices, common_data,    './src-train_dev_0.8_common.txt',   './tgt-train_dev_0.8_common.txt')
+    json2txt(train_dev_indices, uncommon_data,  './src-train_dev_0.8_uncommon.txt', './tgt-train_dev_0.8_uncommon.txt')
+
+def crossValidation(data, train_path, dev_path, test_path, k = 5, k_test=5):
     # Saves k folds
     # k: k fold cross validation
     # k_test: fold to use for test
-    data = json.loads(open(json_path).read())
-    random.shuffle(data)
+
     fold_size = math.floor(np.shape(data)[0] / k)
     for i in range(1, k + 1):
         output = open('fold' + str(i) + '.txt', 'w')
@@ -38,32 +68,21 @@ def crossValidation(json_path, train_path, dev_path, test_path, k = 5, k_test=5)
     for d in train_dev[0:-1000]:
         output.write(d)
     output.close()
+    print(train_path + ' saved')
 
     # Dev
     output = open(dev_path, 'w')
     for d in train_dev[-1000:]:
         output.write(d)
     output.close()
+    print(dev_path + ' saved')
 
     # Test
     output = open(test_path, 'w')
     for d in test:
         output.write(d)
     output.close()
-
-
-def split(json_path, train_path, dev_path, test_path, common=None):
-    data = json.loads(open(json_path).read())
-    random.shuffle(data)
-
-    # remove uncommon equations
-    print('Started with', len(data), 'examples')
-    for d in data:
-        d['segmented_text'], d['equation'] = preprocess(d['segmented_text'], d['equation'])
-    if common is not None:
-        data = mostCommon(data, common)
-    print('Filtered down to', len(data), 'examples')
-
+    print(test_path + ' saved')
 
 def mostCommon(data, percent):
     # returns PERCENT of data by # of equation occurences
@@ -71,22 +90,21 @@ def mostCommon(data, percent):
     equation, count= np.unique([d['equation'] for d in data], return_counts=True)
     indices = np.asarray((equation, count)).T[:,1].astype(int).argsort()
     result = np.asarray([[equation[i], count[i]] for i in indices])
+    removed = np.array([])
 
     total_eqs = np.sum(np.asarray(result[:,1]).astype(int))
-    num_equations_removed = 0
     occurences = 1
-    while num_equations_removed < total_eqs * (1 - percent):
+    while len(removed) < total_eqs * (1 - percent):
         print('Removing equations with', occurences, 'occurences...')
         equations_to_remove = result[:,0][np.asarray(result[:,1]).astype(int) == occurences]
         for eq in equations_to_remove:
             eq = eq.strip()
-            for d in data:
-                if d['equation'].strip() == eq:
-                    data.remove(d)
-                    num_equations_removed += 1
-        print('total # equations removed:', num_equations_removed)
+            removed = np.append(removed, [d for d in data if d['equation'].strip() == eq])
+            data = [d for d in data if not d['equation'].strip() == eq]
+
+        print('total # equations removed:', len(removed))
         occurences += 1
-    return data
+    return data, removed
 
 
 def preprocess(question, equation):
@@ -129,9 +147,7 @@ def preprocess(question, equation):
     equation = ' '.join(equation) + '\n'
     return question, equation
 
-def json2txt(indices_path, json_path, output_path_src, output_path_tgt):
-    json_indices = np.genfromtxt(indices_path).astype(int)
-    data = json.loads(open(json_path).read())
+def json2txt(json_indices, data, output_path_src, output_path_tgt):
     output_src = open(output_path_src, 'w')
     output_tgt = open(output_path_tgt, 'w')
     for d in data:
