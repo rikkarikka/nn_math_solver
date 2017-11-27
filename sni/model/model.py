fddfa046812ae044227ee08818f03a27dfe81610
Ben Robaidek <brobaidek@gmail.com>
2017-11-27 12:26:28 -0800

    flatten params

--- sni/model/model.py
+++ sni/model/model.py
@@ -47,11 +47,11 @@ class Model(nn.Module):
         e = self.emb(inp)
         #e = inp
         if self.net_type == 'lstm':
-            _, (y,_) = self.lstm(e, hc).flatten_parameters()
-            #self.lstm.flatten_parameters()
+            _, (y,_) = self.lstm(e, hc)
+            self.lstm.flatten_parameters()
         elif self.net_type == 'gru':
-            _, y = self.gru(e, hc[0]).flatten_parameters()
-            #self.gru.flatten_parameters()
+            _, y = self.gru(e, hc[0])
+            self.gru.flatten_parameters()
         if self.num_dir==2:
             y = torch.cat([y[0:y.size(0):2], y[1:y.size(0):2]], 2)
         if self.num_layers>1:
