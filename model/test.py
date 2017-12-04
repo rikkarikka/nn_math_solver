c4a735a42968113636cd86ab07cbb262079767a2
Ben Robaidek <brobaidek@gmail.com>
2017-12-04 12:14:30 -0800

    test 0.4

--- model/test.py
+++ model/test.py
@@ -21,8 +21,8 @@ def main():
     LABELS = data.Field(sequential=False)
 
     train, val, test = data.TabularDataset.splits(
-        path='../tencent/data/', train='train_0.6.tsv',
-        validation='train_dev_0.6_common.tsv', test='train_dev_0.6_uncommon.tsv', format='tsv',
+        path='../tencent/data/', train='train_0.8.tsv',
+        validation='train_dev_0.8_common.tsv', test='train_dev_0.8_uncommon.tsv', format='tsv',
         fields=[('text', TEXT), ('label', LABELS)])
 
     TEXT.build_vocab(train)
@@ -32,7 +32,7 @@ def main():
         (train, val, test), batch_sizes=(8, 8, 8),
         sort_key=lambda x: len(x.text), repeat=False)
 
-    model = torch.load('../tencent/models/common_0.6/net-lstm_e25_bs8_opt-adam_ly1_hs300_dr2_ed200_fembFalse_ptembFalse_drp0.3/acc49.00_e6.pt')
+    model = torch.load('../tencent/models/common_0.8/net-lstm_e25_bs8_opt-adam_ly1_hs300_dr2_ed200_fembFalse_ptembFalse_drp0.3/acc47.60_e6.pt')
 
     (avg_loss, accuracy, corrects, size, t5_acc, t5_corrects, mrr) = eval(val_iter, model, TEXT, 300)
     print('COMMON ACCURACY:', accuracy)
