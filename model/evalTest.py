1fdd1c8e01b2dbbca10dd579251a6b3066f45770
Ben Robaidek <brobaidek@gmail.com>
2017-12-19 10:22:36 -0800

    print

--- model/evalTest.py
+++ model/evalTest.py
@@ -25,7 +25,7 @@ def eval(data_iter, model, TEXT, emb_dim, LABELS, snis):
         # Filter predictions based upon SNI
         a = LABELS.vocab.itos
         mask = np.array(snis * batch.batch_size).reshape(batch.batch_size,-1)
-        correct_number_sni = snis[target.data[:]]
+        correct_number_sni = snis[list(target.data)]
         print('correct_number_sni', correct_number_sni)
         print('np.shape(mask)\n', np.shape(mask))
         logit = np.multiply(logit, mask)
