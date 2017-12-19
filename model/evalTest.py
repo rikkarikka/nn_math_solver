77d4f1ae1f34ae470057e0b7f163ef3870db4c36
Ben Robaidek <brobaidek@gmail.com>
2017-12-19 11:01:37 -0800

    print

--- model/evalTest.py
+++ model/evalTest.py
@@ -25,7 +25,7 @@ def eval(data_iter, model, TEXT, emb_dim, LABELS, snis):
         a = LABELS.vocab.itos
         mask = np.array(snis * batch.batch_size).reshape(batch.batch_size,-1)
         correct_number_sni = np.array([snis[i] for i in target.data]).transpose()
-        for i,column in enumerate(mask[,:]):
+        for i,column in enumerate(mask[:,:]):
             print(column)
 
         #logit = np.multiply(logit, mask)
